963972f325b98bff151240d5acecc7c74cc8b127
DeepakVadgama <vadgama.deepak@gmail.com>
2016-07-06 12:26:38 +0530

    CursorAdapter and LoaderManager done.
    Note: Backed out of Cursor with RecyclerView. Not supported natively, and it gets too complicated.

---
+++ app/src/main/java/com/deepakvadgama/radhekrishnabhakti/ContentAdapter.java
@@ -0,0 +1,44 @@
+package com.deepakvadgama.radhekrishnabhakti;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * {@link ContentAdapter} exposes a list of content items
+ * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
+ */
+public class ContentAdapter extends CursorAdapter {
+
+    private boolean mTwoPane = false;
+
+    public ContentAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+
+        // TODO: Is there a way to optimize this infating for each view card?
+        View view = LayoutInflater.from(context).inflate(R.layout.item_list, parent, false);
+        return view;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView tv = (TextView) view;
+//        tv.setText(convertCursorRowToUXFormat(cursor));
+    }
+
+    public boolean ismTwoPane() {
+        return mTwoPane;
+    }
+
+    public void setTwoPane(boolean mTwoPane) {
+        this.mTwoPane = mTwoPane;
+    }
+}
--- app/src/main/java/com/deepakvadgama/radhekrishnabhakti/ItemListActivity.java
+++ app/src/main/java/com/deepakvadgama/radhekrishnabhakti/ItemListActivity.java
@@ -1,23 +1,15 @@
 package com.deepakvadgama.radhekrishnabhakti;
 
-import android.content.Context;
-import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
-import com.deepakvadgama.radhekrishnabhakti.dummy.DummyContent;
-import com.deepakvadgama.radhekrishnabhakti.pojo.Content;
-
-import java.util.List;
+import com.deepakvadgama.radhekrishnabhakti.data.DatabaseContract;
 
 /**
  * An activity representing a list of Items. This activity
@@ -27,13 +19,12 @@ import java.util.List;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class ItemListActivity extends AppCompatActivity {
+public class ItemListActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
+    private static final int CONTENT_LOADER = 0;
     private boolean mTwoPane;
+    private ContentAdapter mContentAdapter;
+    private LoaderManager.LoaderCallbacks<Cursor> mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,98 +35,46 @@ public class ItemListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // TODO: Replace later with Recycler View (though its complicated due lack of native support).
+        // Initialize the adapter. Note that we pass a 'null' Cursor as the third argument. We will pass the adapter a Cursor only when the
+        // data has finished loading for the first time (i.e. when the LoaderManager delivers the data to onLoadFinished). Also note
+        // that we have passed the '0' flag as the last argument. This prevents the adapter from registering a ContentObserver for the
+        // Cursor (the CursorLoader will do this for us!).
+        mContentAdapter = new ContentAdapter(this, null, 0);
 
-        View recyclerView = findViewById(R.id.item_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
-
+        // Tablet
         if (findViewById(R.id.item_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
             mTwoPane = true;
+            mContentAdapter.setTwoPane(true);
         }
+
+        ListView listView = (ListView) findViewById(R.id.item_list);
+        listView.setAdapter(mContentAdapter);
+
+        // Main Sauce - Here loader is created if not present, or already created loader is reused.
+        getSupportLoaderManager().initLoader(CONTENT_LOADER, null, this);
     }
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        // Since activity uses only 1 loader, we are not using id/LOADER_ID
+        // TODO: Check projection and sort order
+        return new CursorLoader(this,
+                DatabaseContract.ContentEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null);
     }
 
-    public class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mContentAdapter.swapCursor(data);
+    }
 
-        private final List<Content> mValues;
-
-        public SimpleItemRecyclerViewAdapter(List<Content> items) {
-            mValues = items;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_list_content, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mItem = mValues.get(position);
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).text);
-
-            holder.mView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mTwoPane) {
-                        Bundle arguments = new Bundle();
-                        arguments.putInt(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-                        ItemDetailFragment fragment = new ItemDetailFragment();
-                        fragment.setArguments(arguments);
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.item_detail_container, fragment)
-                                .commit();
-                    } else {
-                        Context context = v.getContext();
-                        Intent intent = new Intent(context, ItemDetailActivity.class);
-                        intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-
-                        context.startActivity(intent);
-                    }
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return mValues.size();
-        }
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public final View mView;
-            public final TextView mIdView;
-            public final TextView mContentView;
-            public Content mItem;
-
-            public ViewHolder(View view) {
-                super(view);
-                mView = view;
-                mIdView = (TextView) view.findViewById(R.id.id);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
-
-            @Override
-            public String toString() {
-                return super.toString() + " '" + mContentView.getText() + "'";
-            }
-        }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mContentAdapter.swapCursor(null);
     }
 }
