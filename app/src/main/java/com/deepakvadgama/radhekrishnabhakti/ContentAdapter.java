b657556b0faa65fba09841a3fc9f0df1ea176e71
DeepakVadgama <vadgama.deepak@gmail.com>
2016-07-11 15:48:21 +0530

    CardView layout

--- app/src/main/java/com/deepakvadgama/radhekrishnabhakti/ContentAdapter.java
+++ app/src/main/java/com/deepakvadgama/radhekrishnabhakti/ContentAdapter.java
@@ -6,14 +6,30 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * {@link ContentAdapter} exposes a list of content items
- * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
- */
 public class ContentAdapter extends CursorAdapter {
 
     private boolean mTwoPane = false;
+
+    public static class ViewHolder {
+
+        public final ImageView iconView;
+        public final TextView titleView;
+        public final TextView textView;
+        public final ImageButton saveView;
+        public final ImageButton shareView;
+
+        public ViewHolder(View view) {
+            iconView = (ImageView) view.findViewById(R.id.image);
+            titleView = (TextView) view.findViewById(R.id.title);
+            textView = (TextView) view.findViewById(R.id.text);
+            saveView = (ImageButton) view.findViewById(R.id.save);
+            shareView = (ImageButton) view.findViewById(R.id.share);
+        }
+    }
 
     public ContentAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
@@ -22,15 +38,22 @@ public class ContentAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
 
-        // TODO: Is there a way to optimize this infating for each view card?
         View view = LayoutInflater.from(context).inflate(R.layout.item_list_content, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+
         return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-//        TextView tv = (TextView) view;
-//        tv.setText(convertCursorRowToUXFormat(cursor));
+
+        ViewHolder holder = (ViewHolder) view.getTag();
+
+        // Based on type set title or author
+        // Based on type set set image or text
+        // Set click listener on share and save
     }
 
     public boolean ismTwoPane() {
